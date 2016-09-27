82dca53aa726bad8cd3183229f7264d09d56c021
Amirol Ahmad <9w2pdf@gmail.com>
2016-09-28 07:38:32 +0800

    setup devise test and passed it

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryGirl.define do
   factory :user do
-    
+    callsign {|n| "callsign#{n}"}
+    email {|n| "person#{n}@example.com" }
+    password 'secret123'
+    password_confirmation 'secret123'
+    confirmed_at Date.today
+    role 0
+    factory :admin do
+      role 1
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:user)).to be_valid
+  end
+
+  describe 'when user field the form with correct information' do
+
+    it "should be valid with an email" do
+      expect(FactoryGirl.build(:user, email: "test@example.com")).to be_valid
+    end
+
+    it "should be valid with a unique email" do
+      expect(FactoryGirl.build(:user, email: "test@example.com")).to be_valid
+    end
+
+    it "should be valid with an callsign" do
+      expect(FactoryGirl.build(:user, callsign: "9w2pdf")).to be_valid
+    end
+
+    it "should be valid with a unique callsign" do
+      expect(FactoryGirl.build(:user, callsign: "9w2pdf")).to be_valid
+    end
+
+    it "should be valid with a password" do
+      expect(FactoryGirl.build(:user, password: "secret123")).to be_valid
+    end
+
+  end
+
+  describe "when user field the form with incorrect information" do
+
+    it "should be invalid without a email" do
+      expect(FactoryGirl.build(:user, email: nil)).not_to be_valid
+    end
+
+    it "should be invalid without a unique email" do
+      expect(User.find_or_create_by(email: :email)).not_to be_valid
+    end
+
+    it "should be invalid without a callsign" do
+      expect(FactoryGirl.build(:user, callsign: nil)).not_to be_valid
+    end
+
+    it "should be invalid without a unique callsign" do
+      expect(User.find_or_create_by(callsign: :callsign)).not_to be_valid
+    end
+
+    it "should be invalid without a password" do
+      expect(FactoryGirl.build(:user, password: nil)).not_to be_valid
+    end
+
+  end
 end
