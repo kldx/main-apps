b7432621a653f32c982cb1c02973f64e898654cb
Amirol Ahmad <9w2pdf@gmail.com>
2016-09-28 17:18:18 +0800

    add jquery turbolink

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'config'
 gem 'devise'
+gem 'jquery-turbolinks'
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
 //= require skel.min
 //= require util
 //= require main
+//= require turbolinks
