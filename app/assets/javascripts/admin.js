c96b8494330a4d01c2c9b28258cc9d19919d265d
Amirol Ahmad <9w2pdf@gmail.com>
2016-09-29 02:19:34 +0800

    working on their project

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,4 +1,7 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
+//= require react
+//= require react_ujs
+//= require components
 //= require turbolinks
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   include Authenticable
-  
+
   protected
 
   def configure_permitted_parameters
     added_attrs = [:callsign, :email, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description, :default_image, :status)
   end
 end
