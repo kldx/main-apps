9e6cfd89ec6e4510455bb1840fe25474a5d15a92
Amirol Ahmad <9w2pdf@gmail.com>
2016-09-29 13:18:00 +0800

    add crew page

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,5 @@ Rails.application.routes.draw do
   end
 
   get "/dashboard" => 'dashboards#index'
+  get 'crew' => 'pages#crew'
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe PagesController, type: :controller do
     end
   end
 
+  describe "GET #crew" do
+    it "returns http success" do
+      get :crew
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
