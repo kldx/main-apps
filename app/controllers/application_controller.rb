94df78d31b56a10a395e0edbc1799807a78fb0fe
Amirol Ahmad <9w2pdf@gmail.com>
2017-02-18 02:26:10 +0800

    fix deprecated: from to_hash to to_h

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,6 @@ class ApplicationController < ActionController::Base
 
   def set_raven_context
     Raven.user_context(id: session[:current_user]) # or anything else in session
-    Raven.extra_context(params: params.to_hash, url: request.url)
+    Raven.extra_context(params: params.to_h, url: request.url)
   end
 end
