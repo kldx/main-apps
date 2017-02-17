aefb36e94960305b1106e2a9abe2282e79cfddce
Amirol Ahmad <9w2pdf@gmail.com>
2017-02-18 04:45:52 +0800

    configure admin page

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -2,4 +2,28 @@ class Admin::ArticlesController < AdminController
   def index
     @articles = Article.all
   end
+
+  def new
+    #code
+  end
+
+  def create
+    #code
+  end
+
+  def edit
+    #code
+  end
+
+  def update
+    #code
+  end
+
+  def destroy
+    #code
+  end
+
+  private
+
+  
 end
