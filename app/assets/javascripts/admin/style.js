bb501b20a7ca3ca4488c84b2475f742a7e9750fb
Amirol Ahmad <9w2pdf@gmail.com>
2017-02-18 02:25:34 +0800

    styling dashboard

---
+++ app/assets/javascripts/admin/style.js
@@ -0,0 +1,9 @@
+var ready;
+ready = function() {
+
+  /* Add here all your JS customizations */
+
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def body_class
+    "#{controller_name} #{action_name}"
+  end
+
+  def check_dashboard_page
+    false if current_page?(dashboard_path)
+  end
 end
