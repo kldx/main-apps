1cc191e49da68a56a5512de21d210d5a092672a4
Amirol Ahmad <9w2pdf@gmail.com>
2017-02-21 23:31:58 +0800

    change server ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,4 +4,4 @@ set :rails_env, :production
 set :deploy_to, '/home/deploy/kldxteam_production'
 set :release_note_url, 'http://kldxteam.com/release_note.txt'
 
-server '128.199.103.111', user: 'deploy', roles: %w{web app db job}
+server '52.221.37.206', user: 'deploy', roles: %w{web app db job}
